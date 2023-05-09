473870d7fedbc1ac43b76d4d1232fb5adfbef8ef
zefuh <zefuh@iastate.edu>
2023-05-09 18:15:49 -0500

    type: feat Print which epoch stops

--- neurox/interpretation/linear_probe.py
+++ neurox/interpretation/linear_probe.py
@@ -227,7 +227,9 @@ def _train_probe(
         else:
            counter+=1
            if counter >= patience:
+                print(f"Stopping at epoch {epoch+1}")
                return probe
+    print(f"Stopping at epoch {epoch+1}")
     return probe
 
 
